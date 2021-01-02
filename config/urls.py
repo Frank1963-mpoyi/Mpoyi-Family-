00cb15aee2df28c764de372f083030e6c964d44d
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2021-01-02 17:16:21 +0200

    initials

---
+++ config/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path  , include
+
+urlpatterns = [
+    path('', include ('blog.urls')),
+    # path('', include ('AjaxProject.urls', namespace='AjaxProject')),
+    
+    #path('', include ('autenticate.apps.system.users.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
+if settings.DEBUG:
+    # import debug_toolbar
+    
+    # urlpatterns = [
+    #     path('__debug__/', include(debug_toolbar.urls)),
+    #     ]+urlpatterns
+
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
